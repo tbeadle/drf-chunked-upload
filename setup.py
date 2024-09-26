e0a80b662cd05346195cb27d57a6b886296c719a
Tommy Beadle <tbeadle@gmail.com>
2024-09-26 11:32:44 -0400

    Get things ready to start changing code.

--- setup.py
+++ setup.py
@@ -11,23 +11,25 @@ version = os.environ.get("DCU_VERSION", "0.0.0")
 
 
 setup(
-    name="drf-chunked-upload",
+    name="adrf-chunked-upload",
     packages=find_packages("src"),
     package_dir={"": "src"},
     version=version,
     description=(
         "Upload large files to Django REST Framework in multiple chunks,"
-        + " with the ability to resume if the upload is interrupted."
+        " with the ability to resume if the upload is interrupted. Use async Django."
     ),
     long_description=readme,
     long_description_content_type="text/markdown",
-    author="Jarrett Keifer",
-    author_email="jkeifer0@gmail.com",
-    url="https://github.com/jkeifer/drf-chunked-upload",
+    author="Tommy Beadle",
+    author_email="tbeadle@gmail.com",
+    url="https://github.com/tbeadle/adrf-chunked-upload",
     install_requires=[
-        "Django>=2.2",
-        "djangorestframework>=3.11",
+        "Django>=4.1",
+        "djangorestframework>=3.14.0",
+        "adrf>=0.1.7",
+        "aiofiles>=24.1.0",
     ],
-    python_requires=">3.7",
+    python_requires=">3.8",
     license="MIT-Zero",
 )
